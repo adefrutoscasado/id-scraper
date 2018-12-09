f9137ca72cb1f3c0a8103c7872d0cdf4294e1a98
Alejandro de Frutos <adefrutoscasado@gmail.com>
2018-12-09 18:58:41 +0100

    Manage mongo uri enviroment

--- services/databaseService.js
+++ services/databaseService.js
@@ -1,5 +1,7 @@
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
+
 const mongoose = require('mongoose')
-const mongoConnect = () => mongoose.connect('mongodb://localhost:27017')
+const mongoConnect = () => mongoose.connect(MONGODB_URI)
 
 // TODO: Unify expressions get/select
 module.exports = {
